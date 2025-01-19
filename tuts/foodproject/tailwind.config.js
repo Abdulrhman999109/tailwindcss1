d350e8f760f26fa1cd5090e4dc9623f11477d49e
AbdulrhmanAhmed <Abdalrhman999109hotmail.comgit config --global user.email Abdalrhman999109hotmail.com>
2025-01-19 20:37:37 +0300

    style fonts,colors,marigin,padding and border. and config and custom fonts and using flexbox and apply responsive class and style card

--- tuts/foodproject/tailwind.config.js
+++ tuts/foodproject/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.css",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily:{
+          body:['Smooch Sans']
+      }
+    },
   },
   plugins: [],
 }
